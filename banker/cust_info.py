56bb95f6e6457cf89f19d9ce42a8e42b0424a692
prodhanm <mustafizur.prodhan@gmail.com>
2023-11-14 12:08:09 -0500

    instantiated and produced the customer file, by storing data in json file and format.

--- banker/cust_info.py
+++ banker/cust_info.py
@@ -2,7 +2,6 @@ from bank_acct_gen import BankAccountNumber
 import random
 import json
 
-
 class Customer(BankAccountNumber):
     def __init__(self, name, address, city, state, zip_code, phone_number, email):
         self.id = self.generate_id()
@@ -30,10 +29,10 @@ class Customer(BankAccountNumber):
     def get_state(self):
         return self.state   
     
-    def get_zip(self):
+    def get_zip_code(self):
         return self.zip_code
     
-    def get_phone(self):
+    def get_phone_number(self):
         return self.phone_number
     
     def get_email(self):
@@ -49,16 +48,29 @@ customer.get_name()
 customer.get_address()
 customer.get_city()
 customer.get_state()
-customer.get_zip()
-customer.get_phone()
+customer.get_zip_code()
+customer.get_phone_number()
 customer.get_email()
 
+# create a dictionary from the customer object
+customer_dict = {
+    "id": customer.id,
+    "name": customer.name,
+    "address": customer.address,
+    "city": customer.city,
+    "state": customer.state,
+    "zip_code": customer.zip_code,
+    "phone_number": customer.phone_number,
+    "email": customer.email
+}
 
-# create a dictionary of customer instances
-'''customers = {}
-customers[customer.id] = customer
+# create a JSON object from the dictionary
+customer_json = json.dumps(customer_dict)
 
-# print the dictionary of customer instances
-print(customers)
-'''
+# save the JSON object to a file
+with open("customer.json", "w") as f:
+    json.dump(customer_dict, f)
+
+print(customer_json)
+
 
